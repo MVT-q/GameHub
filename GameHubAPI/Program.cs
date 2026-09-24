48cd0d4535a010930554863db62ae069f031f2aa
Yehor Radykop <egorradikop@gmail.com>
2026-09-24 13:13:36 +0200

    feat(auth): configure JWT authentication

--- GameHubAPI/Program.cs
+++ GameHubAPI/Program.cs
@@ -1,8 +1,12 @@
 using GameHubAPI.Data;
 using GameHubAPI.Models;
+using GameHubAPI.Settings;
 using GameHubAPI.Sevices;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace GameHubAPI
 {
@@ -16,10 +20,32 @@ namespace GameHubAPI
 
             builder.Services.AddSwaggerGen();
 
+            var jwtOptions = builder.Configuration
+                .GetSection("Jwt")
+                .Get<JwtOptions>()!;
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = jwtOptions.Issuer,
+                        ValidAudience = jwtOptions.Audience,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.Key))
+                    };
+                });
+
             builder.Services.AddDbContext<AppDbContext>(options =>
             {
                 options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            builder.Services.Configure<JwtOptions>(
+                builder.Configuration.GetSection("Jwt"));
 
             builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
 
@@ -34,6 +60,8 @@ namespace GameHubAPI
             }
 
             app.UseHttpsRedirection();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
